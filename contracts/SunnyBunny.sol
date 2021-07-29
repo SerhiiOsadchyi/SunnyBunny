49b5213a3592c309228b3fb1e14d317aa7514b69
SergiiOsadchyi <serge-o@ukr.net>
2021-07-29 18:08:48 +0300

    Token is working

--- contracts/SunnyBunny.sol
+++ contracts/SunnyBunny.sol
@@ -4,11 +4,16 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SunnyBunny is ERC20, Ownable{
     using SafeMath for uint256;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
     event TokensTransfered (
         address sender,
@@ -21,6 +26,7 @@ contract SunnyBunny is ERC20, Ownable{
     string public _name = "Sunny Bunny";
     string public _symbol = "SuB";
     uint8 private _feePercent;
+    uint256 private _totalSupply;
 
     address internal _feeReciever;
 
@@ -29,7 +35,8 @@ contract SunnyBunny is ERC20, Ownable{
     constructor(address feeReciever, uint8 feePercent) ERC20(_name, _symbol) {
         require(feeReciever != address(0), "Tokens couldn't be transfer to a zero address");
         require(15 >= feePercent, "A fee might to be set to 15% or less");
-        _mint(msg.sender, 7e5 * 1e18);
+        _totalSupply = 7e5 * 1e18;
+        _balances[_msgSender()] = _totalSupply;
         _feeReciever = feeReciever;
         _feePercent = feePercent;
     }
@@ -53,22 +60,36 @@ contract SunnyBunny is ERC20, Ownable{
         _feePercent = feePercent;
     }
 
-    function transferWithFee(address recipient, uint256 amount)
-        public
-        checkAddressIs0(recipient)
-        checkFeePercent(_feePercent)
-        returns (bool) {
-            (uint256 absoluteFee, uint256 amountWithFee) = calculateFee(amount);
+    /**@dev override native ERC20 function */
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+        ) internal override {
+            require(sender != address(0), "ERC20: transfer from the zero address");
+            require(recipient != address(0), "ERC20: transfer to the zero address");
 
-            // transfer tokens to a recipient
-            transfer(owner(), amountWithFee);
-            emit TokensTransfered(msg.sender, recipient, amount, absoluteFee, _feePercent);
+            _beforeTokenTransfer(sender, recipient, amount);
+            uint256 absoluteFee;
+            uint256 amountWithFee;
 
-            // increase inyernal balance of fee's reciver
-            _undisposedFee[_feeReciever] = absoluteFee;
+            if(sender == owner()) {
+                absoluteFee = 0;
+                amountWithFee = amount;
+            } else (absoluteFee, amountWithFee) = calculateFee(amount);
 
-            return true;
-    }
+            uint256 senderBalance = _balances[sender];
+            require(senderBalance >= amountWithFee, "ERC20: transfer amount exceeds balance");
+            unchecked {
+                _balances[sender] = senderBalance - amountWithFee;
+            }
+            _balances[recipient] += amount;
+            _balances[_feeReciever] += absoluteFee;
+
+            emit Transfer(sender, recipient, amount);
+
+            _afterTokenTransfer(sender, recipient, amount);
+        }
 
     function calculateFee (uint256 amount) view internal returns (uint256, uint256){
         uint256 absoluteFee = amount.mul(_feePercent).div(100);
@@ -76,14 +97,213 @@ contract SunnyBunny is ERC20, Ownable{
         return (absoluteFee, amountWithFee);
     }
 
-    function checkFeeBalance(address feeReciever) view public onlyOwner returns(uint) {
-        return _undisposedFee[feeReciever];
+    ///////////  Below standard ERC20 functions almost \\\\\\\\\\\\\
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless this function is
+     * overridden;
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
     }
 
-    // tranfer fee's tokens to a fee's reciever from token's owner
-    function tranferFeeToReciever(address feeReciever) public {
-        transferFrom(owner(), _feeReciever, _undisposedFee[feeReciever]);
-        _undisposedFee[feeReciever] = 0;
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address _owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[_owner][spender];
+    }
+
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * Requirements:
+     *
+     * - `sender` and `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``sender``'s tokens of at least
+     * `amount`.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        unchecked {
+            _approve(sender, _msgSender(), currentAllowance - amount);
+        }
+
+        return true;
+    }
+
+    /**
+     * @dev Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public override returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
+
+    /**
+     * @dev Atomically decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `spender` must have allowance for the caller of at least
+     * `subtractedValue`.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public override returns (bool) {
+        uint256 currentAllowance = _allowances[_msgSender()][spender];
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+        /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal override {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+
+        _afterTokenTransfer(address(0), account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 amount) internal override {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+        }
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+
+        _afterTokenTransfer(account, address(0), amount);
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner` s tokens.
+     *
+     * This internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(
+        address _owner,
+        address spender,
+        uint256 amount
+    ) internal override {
+        require(_owner!= address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[_owner][spender] = amount;
+        emit Approval(owner(), spender, amount);
     }
 
 }
