8eef1b3c03b802311b0ca19693d99a8fb222902d
SergiiOsadchyi <serge-o@ukr.net>
2021-07-29 12:36:59 +0300

    Repair function transferWithFee()

--- contracts/SunnyBunny.sol
+++ contracts/SunnyBunny.sol
@@ -10,11 +10,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract SunnyBunny is ERC20, Ownable{
     using SafeMath for uint256;
 
+    event TokensTransfered (
+        address sender,
+        address recipient,
+        uint amountToken,
+        uint amountFee,
+        uint percentFee
+    );
+
     string public _name = "Sunny Bunny";
     string public _symbol = "SuB";
     uint8 private _feePercent;
 
     address internal _feeReciever;
+
+    mapping(address => uint256) private _undisposedFee;
 
     constructor(address feeReciever, uint8 feePercent) ERC20(_name, _symbol) {
         _mint(msg.sender, 7e5 * 1e18);
@@ -37,15 +47,17 @@ contract SunnyBunny is ERC20, Ownable{
         _feePercent = feePercent;
     }
 
-    function transferWithFee(address recipient, uint256 amount) public checkAddressIs0(recipient) returns (uint256) {
+    function transferWithFee(address recipient, uint256 amount) public checkAddressIs0(recipient) returns (bool) {
         (uint256 absoluteFee, uint256 amountWithFee) = calculateFee(amount);
 
         // transfer tokens to a recipient
-        transfer(recipient, amount);
-        emit Transfer(msg.sender, recipient, amount);
+        transfer(owner(), amountWithFee);
+        emit TokensTransfered(msg.sender, recipient, amount, absoluteFee, _feePercent);
 
-        // transfer a fee to a fee's reciever
-        return tranferFeeToReciever(absoluteFee);
+        // increase inyernal balance of fee's reciver
+        _undisposedFee[_feeReciever] = absoluteFee;
+
+        return true;
     }
 
     function calculateFee (uint256 amount) view internal returns (uint256, uint256){
@@ -54,9 +66,14 @@ contract SunnyBunny is ERC20, Ownable{
         return (absoluteFee, amountWithFee);
     }
 
+    function checkFeeBalance(address feeReciever) view public onlyOwner returns(uint) {
+        return _undisposedFee[feeReciever];
+    }
+
     // tranfer fee's tokens to a fee's reciever from token's owner
-    function tranferFeeToReciever(uint256 feeAmount) public {
-        transferFrom(owner(), _feeReciever, feeAmount);
+    function tranferFeeToReciever(address feeReciever) public {
+        transferFrom(owner(), _feeReciever, _undisposedFee[feeReciever]);
+        _undisposedFee[feeReciever] = 0;
     }
 
 }
